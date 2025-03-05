08a7b1b6c2d5cf134267347dca255a00e8e1ee9e
mauricio celis <andrux430@gmail.com>
2025-03-05 08:49:12 -0500

    create empty project

---
+++ src/main/java/com/example/finalproyect1/Finalproyect1Application.java
@@ -0,0 +1,13 @@
+package com.example.finalproyect1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finalproyect1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Finalproyect1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finalproyect1/Finalproyect1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalproyect1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finalproyect1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
